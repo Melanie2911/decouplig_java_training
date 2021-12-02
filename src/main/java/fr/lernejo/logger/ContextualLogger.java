f41c0e612be42f3e217f0beb2fe5d15b8ca8ac2f
Melanie2911 <m2e9l1a1n2i0e0@gmail.com>
2021-12-02 15:19:15 +0100

    PATH5

---
+++ src/main/java/fr/lernejo/logger/ContextualLogger.java
@@ -0,0 +1,22 @@
+package fr.lernejo.logger;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ContextualLogger implements Logger {
+    private Logger delegateLogger;
+    private DateTimeFormatter formatter;
+    private String callerClass;
+
+    public ContextualLogger(Logger delegateLogger, String callerClass) {
+        this.delegateLogger = delegateLogger;
+        this.formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        this.callerClass = callerClass;
+    }
+
+    @Override
+    public void log(String message) {
+        delegateLogger.log(LocalDateTime.now().format(formatter) + " " + callerClass + " " + message);
+    }
+
+}
--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,7 +2,7 @@ package fr.lernejo.logger;
 
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-       Logger loger = new ConsoleLogger();
+       Logger loger = new ContextualLogger(new FileLogger("fileLog"),name);
        return loger;
     }
 }
